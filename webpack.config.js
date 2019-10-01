3438f2878846b697b7a277aa1a20dde8e7d4f365
phadkead <aditie2005@gmail.com>
2019-10-01 14:46:01 +1000

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
